9db2175c3e0710a4e1bb5856acc3ed781dfc4a12
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-17 12:30:38 +1100

    Added elements trying to set alignments

--- src/main/java/com/wovi10/checklist/ChecklistController.java
+++ src/main/java/com/wovi10/checklist/ChecklistController.java
@@ -1,14 +1,29 @@
 package com.wovi10.checklist;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class ChecklistController {
+    @FXML
+    public VBox vBox;
+    public Group checklist_Group;
+    public Group group_Item;
+    public CheckBox item_CB;
+    public TextField item_Name;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onAddButtonClick() {
+        createNewItem();
+    }
+
+    private void createNewItem() {
+        checklist_Group.getChildren().add(group_Item);
     }
 }
