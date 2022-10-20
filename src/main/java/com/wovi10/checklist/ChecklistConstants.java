d35d91e7517ca79e3f5e070f0251073a0edb6e6e
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-20 18:04:14 +1100

    Refactoring
    extracting more constants

--- src/main/java/com/wovi10/checklist/ChecklistConstants.java
+++ src/main/java/com/wovi10/checklist/ChecklistConstants.java
@@ -2,4 +2,13 @@ package com.wovi10.checklist;
 
 public class ChecklistConstants {
     public static String DELETE_TEXT = "Delete";
+    public static String ADD_TEXT = "Add";
+    public static String CSS_FOLDER = "css/";
+    public static String STRIKETHROUGH_FILE = CSS_FOLDER + "strikethrough.css";
+
+    //region Popup constants
+    public static String POPUP_QUESTION = "What item do you want to add?";
+    public static double POPUP_HEIGHT = 200;
+    public static double POPUP_WIDTH = 300;
+    //endregion
 }
--- src/main/java/com/wovi10/checklist/ChecklistController.java
+++ src/main/java/com/wovi10/checklist/ChecklistController.java
@@ -1,5 +1,6 @@
 package com.wovi10.checklist;
 
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
@@ -15,11 +16,11 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import static com.wovi10.checklist.ChecklistConstants.DELETE_TEXT;
+import static com.wovi10.checklist.ChecklistConstants.*;
 
 public class ChecklistController {
     private final String strikethroughStyle = String.valueOf(
-            ChecklistController.class.getResource("css/strikethrough.css"));
+            ChecklistController.class.getResource(STRIKETHROUGH_FILE));
     @FXML
     public VBox vBox;
     @FXML
@@ -37,14 +38,14 @@ public class ChecklistController {
 
     private String requestItemName() {
         Stage popup = new Stage();
-        Label questionLabel = new Label("What item do you want to add?");
+        Label questionLabel = new Label(POPUP_QUESTION);
         TextField answerField = new TextField();
-        Button answerButton = new Button("Add");
+        Button answerButton = new Button(ADD_TEXT);
         VBox popupContent = new VBox();
 
         popupContent.getChildren().addAll(questionLabel, answerField, answerButton);
         answerButton.setOnAction(e -> giveAnswer(answerField, popup));
-        Scene popupScene = new Scene(popupContent, 300, 200);
+        Scene popupScene = new Scene(popupContent, POPUP_HEIGHT, POPUP_WIDTH);
         popup.setScene(popupScene);
         popup.showAndWait();
         return itemToAdd;
@@ -60,7 +61,7 @@ public class ChecklistController {
         HBox item = new HBox();
         Pane item_spacer = new Pane();
         HBox.setHgrow(item_spacer, Priority.ALWAYS);
-        Label nameLabel = createNameLabel(itemName);
+        Label nameLabel = new Label(itemName);
         CheckBox checkBox = create_CheckBox(nameLabel);
         Button deleteButton = create_DeleteButton(item);
         item.getChildren().addAll(checkBox, nameLabel, item_spacer, deleteButton);
@@ -68,27 +69,22 @@ public class ChecklistController {
     }
 
     private Button create_DeleteButton(HBox item) {
-        Button button = new Button();
-        button.setText(DELETE_TEXT);
+        Button button = new Button(DELETE_TEXT);
         EventHandler<MouseEvent> eventHandler = mouseEvent -> checklist_Group.getChildren().remove(item);
         button.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
         button.setAlignment(Pos.CENTER_RIGHT);
         return button;
     }
 
-    private Label createNameLabel(String itemName) {
-        Label nameLabel = new Label();
-        nameLabel.setText(itemName);
-        return nameLabel;
-    }
-
     private CheckBox create_CheckBox(Label nameLabel) {
         CheckBox checkBox = new CheckBox();
+
         checkBox.selectedProperty().addListener((observableValue, oldValue, newValue) -> {
+            ObservableList<String> nameLabel_Stylesheets = nameLabel.getStylesheets();
             if (newValue) {
-                nameLabel.getStylesheets().add(strikethroughStyle);
+                nameLabel_Stylesheets.add(strikethroughStyle);
             } else {
-                nameLabel.getStylesheets().remove(strikethroughStyle);
+                nameLabel_Stylesheets.remove(strikethroughStyle);
             }
         });
         return checkBox;
