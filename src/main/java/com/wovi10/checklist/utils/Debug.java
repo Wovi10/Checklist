eef5d1fbee89d22c4cf0009b1535ff78427ef813
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-25 15:40:21 +1100

    Update Debug.java

--- src/main/java/com/wovi10/checklist/utils/Debug.java
+++ src/main/java/com/wovi10/checklist/utils/Debug.java
@@ -2,6 +2,8 @@ package com.wovi10.checklist.utils;
 
 public class Debug {
     public static void fileDebug_Print(DebugHelp helper, String location){
-        
+        if (helper.toString().equals("To")){
+            System.out.printf("Writing to: %s", location);
+        }
     }
 }
